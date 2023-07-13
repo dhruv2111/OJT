a9234225c39f025be111574b53563a60d2fc3271
Dhruv Prajapati <dhruvprajapati960@gmail.com>
2023-07-13 13:15:01 +0530

    php completed

---
+++ Day-13/2.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Arya Patel";
+$number = 2101031000130;
+$department = "B.tech IT";
+
+echo "Name: " . $name . "<br>";
+echo "Number: " . $number . "<br>";
+echo "Department: " . $department . "<br>";
+?>
---
+++ Day-13/5.php
@@ -0,0 +1,13 @@
+<?php
+
+    $r=7;
+    $l=5;
+    $w=12;
+
+    echo "Area of circle=".(3.14*$r*$r);
+
+    echo "<br>"."Area of Reactangle=".($l*$w);
+
+
+
+?>
